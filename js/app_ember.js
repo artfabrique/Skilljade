e3d1f485468f8094f23937936b1989ef1efab965
Alexander Vinogradov <artfabrique@gmail.com>
2013-05-19 04:10:41 +0400

    Parse in - Ember out

---
+++ js/app_ember.js
@@ -0,0 +1,17 @@
+var App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
+
+App.UserModel = Ember.Object.extend({
+
+
+
+})
